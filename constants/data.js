3550feb9dd918e7f5699dbcd70461ac77ec0381d
Joshua Akangah <47978604+kangah-codes@users.noreply.github.com>
2021-10-17 15:57:37 +0000

    chore: Added more dummy data

---
+++ constants/data.js
@@ -0,0 +1,180 @@
+export const discounts = [
+	{
+		id: "1",
+		name: "Grab & Go Foods East Legon",
+		price: "3.00",
+		rating: "3.0",
+		discount: "50%",
+		image: require("../assets/images/food/fried-rice.jpg"),
+	},
+	{
+		id: "2",
+		name: "Aseda Restaurant",
+		price: "4.00",
+		rating: "3.0",
+		discount: "50%",
+		image: require("../assets/images/food/pizza.jpg"),
+	},
+	{
+		id: "3",
+		name: "KFC East Legon",
+		price: "2.00",
+		rating: "4.0",
+		discount: "50%",
+		image: require("../assets/images/food/chicken.jpg"),
+	},
+	{
+		id: "4",
+		name: "Rocomamas",
+		price: "5.00",
+		rating: "4.5",
+		discount: "50%",
+		image: require("../assets/images/food/rocomamas.jpg"),
+	},
+];
+
+export const restaurants = [
+	{
+		id: "1",
+		name: "Grab & Go Foods East Legon",
+		price: "3.00",
+		rating: "3.0",
+		discount: "50%",
+		image: require("../assets/images/food/fried-rice.jpg"),
+	},
+	{
+		id: "2",
+		name: "Aseda Restaurant That this is the name of the place soo much",
+		price: "4.00",
+		rating: "3.0",
+		image: require("../assets/images/food/pizza.jpg"),
+	},
+	{
+		id: "3",
+		name: "KFC East Legon",
+		price: "2.00",
+		rating: "4.0",
+		image: require("../assets/images/food/chicken.jpg"),
+	},
+	{
+		id: "4",
+		name: "Rocomamas",
+		price: "5.00",
+		rating: "4.5",
+		discount: "50%",
+		image: require("../assets/images/food/rocomamas.jpg"),
+	},
+];
+
+export const popularFoods = [
+	"Fast Food",
+	"Chinese",
+	"Italian",
+	"Japanese",
+	"Thai",
+	"Indian",
+	"Korean",
+	"Vietnamese",
+	"Malaysian",
+	"Mediterranean",
+	"Mexican",
+	"Middle Eastern",
+	"Pizza",
+	"Seafood",
+	"Steak",
+	"Sushi",
+	"Vegetarian",
+	"Vegan",
+	"Venezuelan",
+];
+
+export const myOrders = [
+	{
+		id: "1",
+		name: "Grab & Go Foods East Legon",
+		price: "3.00",
+		date: "13 Oct, 2021 8:52 PM",
+		status: "Delivered",
+		image: require("../assets/images/food/fried-rice.jpg"),
+	},
+	{
+		id: "2",
+		name: "Aseda Restaurant",
+		price: "4.00",
+		date: "13 Oct, 2021 8:52 PM",
+		status: "Delivered",
+		image: require("../assets/images/food/pizza.jpg"),
+	},
+	{
+		id: "3",
+		name: "KFC East Legon",
+		price: "2.00",
+		date: "13 Oct, 2021 8:52 PM",
+		status: "Delivered",
+		image: require("../assets/images/food/chicken.jpg"),
+	},
+	{
+		id: "4",
+		name: "Rocomamas",
+		price: "5.00",
+		date: "13 Oct, 2021 8:52 PM",
+		status: "Delivered",
+		image: require("../assets/images/food/rocomamas.jpg"),
+	},
+
+	{
+		id: "5",
+		name: "Grab & Go Foods East Legon",
+		price: "3.00",
+		date: "13 Oct, 2021 8:52 PM",
+		status: "Delivered",
+		image: require("../assets/images/food/fried-rice.jpg"),
+	},
+	{
+		id: "6",
+		name: "Aseda Restaurant",
+		price: "4.00",
+		date: "13 Oct, 2021 8:52 PM",
+		status: "Delivered",
+		image: require("../assets/images/food/pizza.jpg"),
+	},
+	{
+		id: "7",
+		name: "KFC East Legon",
+		price: "2.00",
+		date: "13 Oct, 2021 8:52 PM",
+		status: "Delivered",
+		image: require("../assets/images/food/kfc.png"),
+	},
+	{
+		id: "8",
+		name: "Rocomamas",
+		price: "5.00",
+		date: "13 Oct, 2021 8:52 PM",
+		status: "Delivered",
+		image: require("../assets/images/food/burger.jpg"),
+	},
+];
+
+export const profileTabs = [
+	{
+		id: "1",
+		name: "Payment",
+		icon: "creditcard",
+	},
+	{
+		id: "2",
+		name: "Settings",
+		icon: "ios-settings-outline",
+	},
+	{
+		id: "4",
+		name: "About",
+		icon: "information-circle-outline",
+	},
+	{
+		id: "3",
+		name: "Help",
+		icon: "ios-help-circle-outline",
+	},
+];
