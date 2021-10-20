96d39d8e9e047a8f532c4876c8df365a29883b56
Joshua Akangah <47978604+kangah-codes@users.noreply.github.com>
2021-10-20 16:28:12 +0000

    feat: Added new data properties

--- constants/data.js
+++ constants/data.js
@@ -41,13 +41,129 @@ export const restaurants = [
 		rating: "3.0",
 		discount: "50%",
 		image: require("../assets/images/food/fried-rice.jpg"),
+		menu: [
+			{
+				id: "1",
+				name: "Rice Meals",
+				items: [
+					{
+						id: "1",
+						name: "Fried Rice",
+						price: "3.00",
+					},
+					{
+						id: "2",
+						name: "Jollof Rice",
+						price: "3.00",
+					},
+					{
+						id: "3",
+						name: "Curry Rice",
+						price: "3.00",
+					},
+					{
+						id: "4",
+						name: "Steamed Rice",
+						price: "3.00",
+					},
+				],
+			},
+			{
+				id: "2",
+				name: "Noodles",
+				items: [
+					{
+						id: "1",
+						name: "Chicken Indomie",
+						price: "3.00",
+					},
+					{
+						id: "2",
+						name: "Beef Indomie",
+						price: "3.00",
+					},
+					{
+						id: "3",
+						name: "Pork Indomie",
+						price: "3.00",
+					},
+					{
+						id: "4",
+						name: "Vegetable Noodles",
+						price: "3.00",
+					},
+				],
+			},
+		],
 	},
 	{
 		id: "2",
-		name: "Aseda Restaurant That this is the name of the place soo much",
+		name: "Aseda Restaurant",
 		price: "4.00",
 		rating: "3.0",
 		image: require("../assets/images/food/pizza.jpg"),
+		menu: [
+			{
+				id: "1",
+				name: "Pizza Combo",
+				items: [
+					{
+						id: "1",
+						name: "Meat Lovers Pizza",
+						price: "38.00",
+						image: require("../assets/images/restaurant_foods/meat-lovers.jpg"),
+					},
+					{
+						id: "2",
+						name: "Hawaian Pizza",
+						price: "34.00",
+						image: require("../assets/images/restaurant_foods/hawaian.jpg"),
+					},
+					{
+						id: "3",
+						name: "Fully Loaded Pizza",
+						price: "39.00",
+						image: require("../assets/images/restaurant_foods/fully-loaded.jpg"),
+					},
+					{
+						id: "4",
+						name: "Pepperoni Pizza",
+						price: "76.00",
+						image: require("../assets/images/restaurant_foods/pepperoni.jpg"),
+					},
+				],
+			},
+			{
+				id: "2",
+				name: "Burgers",
+				items: [
+					{
+						id: "1",
+						name: "Beef Burger",
+						price: "3.00",
+						image: require("../assets/images/restaurant_foods/beef-burger.jpg"),
+					},
+					{
+						id: "2",
+						name: "Chicken Burgers",
+						price: "3.00",
+						image: require("../assets/images/restaurant_foods/chicken-burger.jpg"),
+					},
+					{
+						id: "3",
+						name: "Vegan Burger",
+						price: "3.00",
+						image: require("../assets/images/restaurant_foods/vegan-burger.jpg"),
+					},
+					{
+						id: "4",
+						name: "Fish Filet Burger",
+						price: "3.00",
+						image: require("../assets/images/restaurant_foods/fish-burger.jpg"),
+					},
+				],
+			},
+		],
 	},
 	{
 		id: "3",
@@ -55,6 +171,77 @@ export const restaurants = [
 		price: "2.00",
 		rating: "4.0",
 		image: require("../assets/images/food/chicken.jpg"),
+		menu: [
+			{
+				id: "1",
+				name: "Streetwise Meals",
+				items: [
+					{
+						id: "1",
+						name: "Streetwise 3",
+						description: "Chicken, Beef, Pork, Vegetable",
+						price: "3.00",
+						image: require("../assets/images/restaurant_foods/streetwise-3.jpg"),
+					},
+					{
+						id: "2",
+						name: "Streetwise 10",
+						description: "Chicken, Beef, Pork, Vegetable",
+						price: "3.00",
+						image: require("../assets/images/restaurant_foods/streetwise-10.jpg"),
+					},
+					{
+						id: "3",
+						name: "Streetwise 2 with Mini Loaf",
+						description: "Chicken, Beef, Pork, Vegetable",
+						price: "3.00",
+						image: require("../assets/images/restaurant_foods/streetwise-2.jpg"),
+					},
+					{
+						id: "4",
+						name: "Streetwise 5",
+						description: "Chicken, Beef, Pork, Vegetable",
+						price: "3.00",
+						image: require("../assets/images/restaurant_foods/streetwise-5.jpg"),
+					},
+				],
+			},
+			{
+				id: "2",
+				name: "Chicken Pieces and Chicken Buckets",
+				items: [
+					{
+						id: "1",
+						name: "1 piece chicken",
+						description: "Chicken",
+						price: "3.00",
+						discount: "1.50",
+						image: require("../assets/images/restaurant_foods/1-piece.jpg"),
+					},
+					{
+						id: "2",
+						name: "2 Piece chicken",
+						description: "Chicken",
+						price: "3.00",
+						image: require("../assets/images/restaurant_foods/2-piece.jpg"),
+					},
+					{
+						id: "3",
+						name: "12 piece bucket",
+						description: "Chicken",
+						price: "3.00",
+						image: require("../assets/images/restaurant_foods/12-piece.jpg"),
+					},
+					{
+						id: "4",
+						name: "18 piece chicken",
+						description: "Chicken",
+						price: "3.00",
+						image: require("../assets/images/restaurant_foods/18-piece.jpg"),
+					},
+				],
+			},
+		],
 	},
 	{
 		id: "4",
@@ -63,6 +250,56 @@ export const restaurants = [
 		rating: "4.5",
 		discount: "50%",
 		image: require("../assets/images/food/rocomamas.jpg"),
+		menu: [
+			{
+				id: "1",
+				name: "Smashed",
+				items: [
+					{
+						id: "1",
+						name: "Chilli Cheese Smashed burger",
+						description: "Chicken",
+						price: "3.00",
+						discount: "50.00",
+						image: require("../assets/images/restaurant_foods/bacon-burger.jpg"),
+					},
+					{
+						id: "2",
+						name: "Bacon, Cheese and Guacamole burger",
+						description: "Chicken",
+						price: "3.00",
+						image: require("../assets/images/restaurant_foods/bacon-burger.jpg"),
+					},
+					{
+						id: "3",
+						name: "Mushroom Burger",
+						description: "Chicken",
+						price: "3.00",
+						image: require("../assets/images/restaurant_foods/mushroom-burger.jpg"),
+					},
+					{
+						id: "4",
+						name: "Cheese and Bacon burger",
+						description: "Chicken",
+						price: "3.00",
+						image: require("../assets/images/restaurant_foods/bacon-burger.jpg"),
+					},
+				],
+			},
+			{
+				id: "2",
+				name: "Black Line Burgers",
+				items: [
+					{
+						id: "1",
+						name: "Blackstar burger",
+						description: "Chicken",
+						price: "3.00",
+						image: require("../assets/images/restaurant_foods/blackstar-burger.jpg"),
+					},
+				],
+			},
+		],
 	},
 ];
 
